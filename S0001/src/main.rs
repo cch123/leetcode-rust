ebe158461c6dc964395c385d19c3da89f33693b0
Xargin <cao1988228@163.com>
2018-12-21 17:15:38 +0800

    add 0001

--- S0001/src/main.rs
+++ S0001/src/main.rs
@@ -4,28 +4,19 @@ struct Solution {}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut h:HashMap<i32,i32>= HashMap::new();
-        let mut res = vec![];
-        for (idx,i) in nums.iter().enumerate() {
-            let resv = target - i;
-            match h.get(&resv) {
-                Some(v) => {
-                    res.push(*v);
-                    res.push(idx as i32);
-                    return res;
-                },
-                None => {
-                    h.insert(*i,idx as i32);
-                    continue;
-                },
-            }
+        let mut h: HashMap<i32, i32> = HashMap::new();
+        for (idx, i) in nums.iter().enumerate() {
+            let left = target - i;
+            match h.get(&left) {
+                Some(v) => return vec![*v, idx as i32],
+                None => h.insert(*i, idx as i32),
+            };
         }
-        println!("{:?}", h);
-        return res;
+        return vec![];
     }
 }
 
 fn main() {
-    let v = vec![1,2,3,4];
-    println!("{:?}",Solution::two_sum(v, 5));
+    let v = vec![1, 2, 3, 4];
+    println!("{:?}", Solution::two_sum(v, 5));
 }
