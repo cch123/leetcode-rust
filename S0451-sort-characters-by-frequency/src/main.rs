69bbd319fa61d8f7304fefb7a7ea3ce0696d4b7a
Xargin <cao1988228@163.com>
2019-01-16 14:42:49 +0800

    remove unsafe

--- S0451-sort-characters-by-frequency/src/main.rs
+++ S0451-sort-characters-by-frequency/src/main.rs
@@ -1,30 +1,26 @@
 struct Solution;
 
-use std::collections::HashMap;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 impl Solution {
     pub fn frequency_sort(s: String) -> String {
         let mut char_cnt = HashMap::new();
-        s.bytes().for_each(|c|{
-            char_cnt.entry(c).and_modify(|e|*e+=1).or_insert(1);
+        s.bytes().for_each(|c| {
+            char_cnt.entry(c).and_modify(|e| *e += 1).or_insert(1);
         });
 
-        let mut s = s;
-        unsafe {
-            let mut z = s.as_bytes_mut();
-            z.sort_by(|x, y| {
-                if char_cnt.get(x).unwrap_or(&0) > char_cnt.get(y).unwrap_or(&0) {
-                    return Ordering::Less;
-                }
-                if char_cnt.get(x).unwrap_or(&0) < char_cnt.get(y).unwrap_or(&0) {
-                    return Ordering::Greater;
-                }
-                return x.cmp(y)
-            });
-            return String::from_utf8(z.to_vec()).unwrap();
-        }
+        let mut byte_arr = s.into_bytes();
+        byte_arr.sort_by(|x, y| {
+            if char_cnt.get(x).unwrap_or(&0) > char_cnt.get(y).unwrap_or(&0) {
+                return Ordering::Less;
+            }
+            if char_cnt.get(x).unwrap_or(&0) < char_cnt.get(y).unwrap_or(&0) {
+                return Ordering::Greater;
+            }
+            return x.cmp(y);
+        });
 
-        "".to_string()
+        return String::from_utf8(byte_arr).unwrap();
     }
 }
 
