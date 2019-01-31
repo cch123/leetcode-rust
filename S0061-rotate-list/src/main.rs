69a0e8ebf9a404ec3a07b767d46e6f8f036cc9b8
Xargin <cao1988228@163.com>
2019-01-31 22:36:47 +0800

    add 61

--- S0061-rotate-list/src/main.rs
+++ S0061-rotate-list/src/main.rs
@@ -49,3 +49,71 @@ fn main() {
     println!("{:?}", Solution::rotate_right(l, 1));
 }
 
+/*
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        use std::mem::swap;
+        
+        let len = Solution::len(&head);
+        if k == 0 || len == 0 || k as usize % len == 0 {
+            return head;
+        }
+        
+        let mut node = &mut head;
+        for _ in 0..(len - (k as usize % len) - 1) {
+            if let Some(ref mut n) = node {
+                node = &mut n.next;
+            } 
+        }
+        
+        let mut new_head = None;
+        if let Some(ref mut node) = node {
+            swap(&mut node.next, &mut new_head);
+        }
+        
+        // we have now cut the linked list in half
+        // now we have to put it back together backwards
+        
+        if let Some(ref mut new_head) = new_head {
+            let mut node = new_head;
+            while let Some(ref mut next) = node.next {
+                node = next;
+            }
+            node.next = head;
+        }
+        
+        new_head
+    }
+    
+    fn len(head: &Option<Box<ListNode>>) -> usize {
+        if let Some(ref n) = head {
+            let mut len = 1;
+            let mut node = n;
+            while let Some(ref next) = node.next {
+                len += 1;
+                node = next;
+            }
+            len
+        }
+        else {
+            0
+        }
+    }
+}
+*/
