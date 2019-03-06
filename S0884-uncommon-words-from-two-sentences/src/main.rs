57e76cf754a6b5f3e719ea6371629c2d99c3c07d
Xargin <cao1988228@163.com>
2019-03-06 13:33:39 +0800

    add filter_map

--- S0884-uncommon-words-from-two-sentences/src/main.rs
+++ S0884-uncommon-words-from-two-sentences/src/main.rs
@@ -12,7 +12,20 @@ impl Solution {
         let word_arr = line.split(" ").collect::<Vec<&str>>();
 
         let mut counter = HashMap::new();
-        word_arr.iter().for_each(|w| { counter.entry(w).and_modify(|e| *e += 1).or_insert(1); });
-        counter.iter().filter(|(_, &c)| c == 1).map(|(w, _)| w.to_string()) .collect::<Vec<String>>()
+        word_arr.iter().for_each(|w| {
+            counter.entry(w).and_modify(|e| *e += 1).or_insert(1);
+        });
+        counter
+            .iter()
+            .filter(|(_, &c)| c == 1)
+            .map(|(w, _)| w.to_string())
+            .collect::<Vec<String>>()
+
+        /* 合并为 filter_map 也可以
+        counter
+            .iter()
+            .filter_map(|(w, &c)| if c == 1 { Some(w.to_string()) } else { None })
+            .collect::<Vec<String>>()
+        */
     }
 }
